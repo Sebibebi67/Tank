0a4cee70cfeeecfa2d108c0f335840d4fdb133ae
Sebibebi67 <sebibebi67@gmail.com>
2019-11-08 19:19:43 +0100

    checkcollision made with Area

--- client/Shot.java
+++ client/Shot.java
@@ -2,40 +2,40 @@ package client;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.geom.Ellipse2D;
+import java.awt.Rectangle;
 public class Shot{
     
-    private int x,y;
+    private double x,y;
     private double alpha;
-    private int speed;
-    private int shotSize;
-    private Ellipse2D circle;
+    private double speed;
+    private double shotSize;
+    private Rectangle rect;
 
-    public Shot(int x, int y, double alpha, SPanel panel){
+    public Shot(double x, double y, double alpha, SPanel panel){
         this.x = x;
         this.y = y;
         this.alpha = alpha;
         this.speed = 15;
         this.shotSize = 7;
-        this.circle = newCircle();
+        this.rect = newrect();
     }
 
     public void display(Graphics g){
         g.setColor(Color.BLACK);
 
         g.setColor(Color.WHITE);
-        g.fillOval(x-shotSize/2, y-shotSize/2, shotSize, shotSize);
+        g.fillOval((int)(x-shotSize/2), (int)(y-shotSize/2), (int)shotSize, (int)shotSize);
     }
 
     public void update(double diff){
-        this.x = (int) (this.x + Math.cos(this.alpha)*this.speed*diff);
-        this.y = (int) (this.y + Math.sin(this.alpha)*this.speed*diff);
-        this.circle = newCircle();
+        this.x = this.x + Math.cos(this.alpha)*this.speed*diff;
+        this.y = this.y + Math.sin(this.alpha)*this.speed*diff;
+        this.rect = newrect();
     }
 
-    public Ellipse2D newCircle(){
-        return new Ellipse2D.Float(x-shotSize/2, y-shotSize/2, shotSize, shotSize);
+    public Rectangle newrect(){
+        return new Rectangle((int)(x-shotSize/2), (int)(y-shotSize/2), (int)shotSize, (int)shotSize);
     }
 
-    public Ellipse2D getShape(){return circle;}
+    public Rectangle getShape(){return rect;}
 }
