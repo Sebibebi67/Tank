a588fee3c286836ba906c7beb7e562d9b51cdf19
TonyChouteau <tony.chouteau@outlook.fr>
2019-11-09 01:49:58 +0100

    adding game room management (server side)

--- server/Room.java
+++ server/Room.java
@@ -1,30 +1,49 @@
 package server;
 
 import entities.*;
-import java.awt.*;
 
+import java.awt.Dimension;
 import java.util.ArrayList;
 
 public class Room {
 
     private final int MAX_PLAYER = 4;
 
-    private static int newRoomId = 0;
     private final int id;
 
     private Map map;
+
     private ArrayList<Player> players;
 
     Dimension size = new Dimension(1600, 830);
 
     // Management room object
 
-    public Room() {
-        this.id = newRoomId;
-        newRoomId++;
+    public Room(int roomID, int playerID) {
+        this.id = roomID;
+
+        map = new Map();
+
+        players.add(new Player(id, 500,500, map));
     }
 
     public int getId() {
 		return this.id;
-	}
+    }
+
+    public Map getMap() {
+        return this.map;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return this.players;
+    }
+
+    /*public void setPlayers(Object players) {
+        this.players = players;
+    }*/
+
+    /*public void setMap(Map map) {
+        this.map = map;
+    }*/
 }
